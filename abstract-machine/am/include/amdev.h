d4975c82d7f6d4660e5b41c435744c9fc901a07b
ysyx_25040118-Pu Zhuo <1787369006@qq.com>
2026-02-08 22:02:41 +0800

    修复了游离HEAD指针的问题，之前的提交完成了PA2,准备尝试C5（之前还在am-kernel/kernel/下啊增加了ikun的从事程序！）

--- abstract-machine/am/include/amdev.h
+++ abstract-machine/am/include/amdev.h
@@ -7,30 +7,50 @@
   enum { AM_##reg = (id) }; \
   typedef struct { __VA_ARGS__; } AM_##reg##_T;
 
-AM_DEVREG( 1, UART_CONFIG,  RD, bool present);
-AM_DEVREG( 2, UART_TX,      WR, char data);
-AM_DEVREG( 3, UART_RX,      RD, char data);
-AM_DEVREG( 4, TIMER_CONFIG, RD, bool present, has_rtc);
-AM_DEVREG( 5, TIMER_RTC,    RD, int year, month, day, hour, minute, second);
-AM_DEVREG( 6, TIMER_UPTIME, RD, uint64_t us);
-AM_DEVREG( 7, INPUT_CONFIG, RD, bool present);
-AM_DEVREG( 8, INPUT_KEYBRD, RD, bool keydown; int keycode);
-AM_DEVREG( 9, GPU_CONFIG,   RD, bool present, has_accel; int width, height, vmemsz);
-AM_DEVREG(10, GPU_STATUS,   RD, bool ready);
-AM_DEVREG(11, GPU_FBDRAW,   WR, int x, y; void *pixels; int w, h; bool sync);
-AM_DEVREG(12, GPU_MEMCPY,   WR, uint32_t dest; void *src; int size);
-AM_DEVREG(13, GPU_RENDER,   WR, uint32_t root);
-AM_DEVREG(14, AUDIO_CONFIG, RD, bool present; int bufsize);
-AM_DEVREG(15, AUDIO_CTRL,   WR, int freq, channels, samples);
-AM_DEVREG(16, AUDIO_STATUS, RD, int count);
-AM_DEVREG(17, AUDIO_PLAY,   WR, Area buf);
-AM_DEVREG(18, DISK_CONFIG,  RD, bool present; int blksz, blkcnt);
-AM_DEVREG(19, DISK_STATUS,  RD, bool ready);
-AM_DEVREG(20, DISK_BLKIO,   WR, bool write; void *buf; int blkno, blkcnt);
-AM_DEVREG(21, NET_CONFIG,   RD, bool present);
-AM_DEVREG(22, NET_STATUS,   RD, int rx_len, tx_len);
-AM_DEVREG(23, NET_TX,       WR, Area buf);
-AM_DEVREG(24, NET_RX,       WR, Area buf);
+
+//抽象机器设备寄存器定义, 包括寄存器ID, 读写权限, 以及寄存器内容、格式等信息
+AM_DEVREG( 1, UART_CONFIG,  RD, bool present);//串口配置
+                                //是否存在串口设备
+AM_DEVREG( 2, UART_TX,      WR, char data);//串口发送数据
+                                //要发送的1字节字符
+AM_DEVREG( 3, UART_RX,      RD, char data);//串口接收数据
+                                //从串口接受到的1字节字符
+AM_DEVREG( 4, TIMER_CONFIG, RD, bool present, has_rtc);//定时器配置
+                                //读年月日秒    //运行时间
+AM_DEVREG( 5, TIMER_RTC,    RD, int year, month, day, hour, minute, second);//实时时钟
+                                //完整日历
+AM_DEVREG( 6, TIMER_UPTIME, RD, uint64_t us);//定时器相关
+                                //系统启动以来的时长
+AM_DEVREG( 7, INPUT_CONFIG, RD, bool present);//输入设备配置
+                                //是否有输入设备作用
+AM_DEVREG( 8, INPUT_KEYBRD, RD, bool keydown; int keycode);//键盘输入
+                                //是否按下按键  //扫描码
+AM_DEVREG( 9, GPU_CONFIG,   RD, bool present, has_accel; int width, height, vmemsz);                 //AM显示控制器信息, 可读出屏幕大小信息width和height，
+                                //是否存在GPU  //支持硬件加速//宽      //高    //显存可用内存大小           //另外AM假设系统在运行过程中, 屏幕大小不会发生变化.
+AM_DEVREG(10, GPU_STATUS,   RD, bool ready);//图形处理单元状态
+                                //GPU是否就绪
+AM_DEVREG(11, GPU_FBDRAW,   WR, int x, y; void *pixels; int w, h; bool sync);                        //写入绘图信息向屏幕(x, y)坐标处绘制w*h的矩形图像,
+                                //左上角坐标//像素数据指针  //画的宽高 //立即刷新到屏幕                     //图像像素按行优先方式存储在pixels中, 每个像素用32位整数
+                                                                                                     //以00RRGGBB的方式描述颜色. 若sync为true, 则马上将帧缓冲中的内容同步到屏幕上.
+AM_DEVREG(12, GPU_MEMCPY,   WR, uint32_t dest; void *src; int size);//图形处理单元内存拷贝
+                                //GPU侧目标地址偏移，显存地址
+AM_DEVREG(13, GPU_RENDER,   WR, uint32_t root);//图形处理单元渲染
+                                //出发一次渲染
+AM_DEVREG(14, AUDIO_CONFIG, RD, bool present; int bufsize);//音频配置
+                                //有没有音频设备//流缓冲区大小
+AM_DEVREG(15, AUDIO_CTRL,   WR, int freq, channels, samples);//音频控制
+                                //采样率   //声道数   //播放的周期长度
+AM_DEVREG(16, AUDIO_STATUS, RD, int count);//音频状态
+                                //流缓冲区内已写入的字节长度
+AM_DEVREG(17, AUDIO_PLAY,   WR, Area buf);//音频播放
+                                //要播放的音频数据去见
+AM_DEVREG(18, DISK_CONFIG,  RD, bool present; int blksz, blkcnt);//磁盘配置
+AM_DEVREG(19, DISK_STATUS,  RD, bool ready);//磁盘状态
+AM_DEVREG(20, DISK_BLKIO,   WR, bool write; void *buf; int blkno, blkcnt);//磁盘块输入输出
+AM_DEVREG(21, NET_CONFIG,   RD, bool present);//网络配置
+AM_DEVREG(22, NET_STATUS,   RD, int rx_len, tx_len);//网络状态
+AM_DEVREG(23, NET_TX,       WR, Area buf);//网络发送
+AM_DEVREG(24, NET_RX,       WR, Area buf);//网络接收
 
 // Input
 
